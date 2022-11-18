402ead03893d45c029bbef0f6decedf6faa4f27d
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-11-18 14:52:03 -0700

    Added test for the getOfficeNumber method

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -8,4 +8,15 @@ describe('Manager', () => {
         expect(manager).toEqual({ name:'Ms Piggy', id: '347658', email: 'ms.piggy@bosslady.com', officeNumber: '80' });
     });
 
+    describe('Get Office Number', () => {
+
+        it('should return the office number of the manager', () => {
+            const manager = new Manager('Ms Piggy', '347658', 'ms.piggy@bosslady.com', '80');
+
+            const result = manager.officeNumber();
+
+            expect(result).toBe('80');            
+        });
+    });
+
 });
